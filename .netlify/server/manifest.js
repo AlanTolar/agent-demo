a4d4e54b499e25df12653a1e60138643c9239781
Alan <test@test>
2022-10-18 14:47:50 -0400

    cms identity widget

--- .netlify/server/manifest.js
+++ .netlify/server/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set([".DS_Store","admin/config.yml","admin/index.html","favicon.png"]),
 	mimeTypes: {".yml":"text/yaml",".html":"text/html",".png":"image/png"},
 	_: {
-		entry: {"file":"_app/immutable/start-86b7d85a.js","imports":["_app/immutable/start-86b7d85a.js","_app/immutable/chunks/index-edd250b0.js","_app/immutable/chunks/singletons-cc4cbb96.js"],"stylesheets":[]},
+		entry: {"file":"_app/immutable/start-46e38ccf.js","imports":["_app/immutable/start-46e38ccf.js","_app/immutable/chunks/index-edd250b0.js","_app/immutable/chunks/singletons-10374625.js"],"stylesheets":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-40cc45d3.js';
-export const imports = ["_app/immutable/components/error.svelte-40cc45d3.js","_app/immutable/chunks/index-edd250b0.js","_app/immutable/chunks/singletons-cc4cbb96.js"];
+export const file = '_app/immutable/components/error.svelte-1ff5f726.js';
+export const imports = ["_app/immutable/components/error.svelte-1ff5f726.js","_app/immutable/chunks/index-edd250b0.js","_app/immutable/chunks/singletons-10374625.js"];
 export const stylesheets = [];
