6523d8b3b3da15699bc625e0adb3ae8a5a7cba31
Alan <test@test>
2022-10-18 21:33:36 -0400

    media uploads

---
+++ src/routes/listings/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const modules = import.meta.glob('./*.json', { eager: true });
+	const listings = Object.values(modules);
+	console.log(listings);
+</script>
+
+<h1>Listings</h1>
+{#each listings as listing}
+	<li>
+		<span>{listing.title} - {listing.body}</span>
+	</li>
+{/each}
