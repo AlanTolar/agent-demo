dbe46483295aefba4dc0be0c40e8bb754bae4e8c
Alan <test@test>
2022-12-07 22:54:54 -0500

    added to homepage and updated netlify cms config

---
+++ src/lib/scripts/listings.ts
@@ -0,0 +1,15 @@
+
+
+function getListings(num=10) {
+    const modules = import.meta.glob('$lib/content/listings/*.json', { eager: true });
+    console.log(modules)
+    const listings = Object.keys(modules).map((key) => {
+        const filename = key.split('/').at(-1)?.split('.').at(0);
+        const url = `/listings/${filename}`;
+        return { ...modules[key], json_url: key, url };
+    });
+    return listings.slice(0,num)    
+}
+
+
+export {getListings}
---
+++ src/routes/(app)/listings/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getListings } from '$lib/scripts/listings';
+	const listings = getListings(3);
+</script>
+
+<h1>Listings</h1>
+<div class="grid grid-cols-1 divide-y">
+	{#each listings as listing}
+		<div class="p-5">
+			<a href="{listing.url}" class="text-xl font-bold">{listing.title}</a>
+			<p class="mt-1 text-sm">{listing.body}</p>
+			<img src="{listing.thumbnail}" alt="" class="h-40" />
+		</div>
+	{/each}
+</div>
