242b9b3a62c3bcdc3094fcc7bb7ef8c7d9dbadcb
Alan <test@test>
2022-12-10 13:38:20 -0500

    config upadte and listing detail changes

--- src/routes/(app)/listings/[listing]/+page.svelte
+++ src/routes/(app)/listings/[listing]/+page.svelte
@@ -1,51 +1,51 @@
 <script lang="ts">
-	import Markdown from '@magidoc/plugin-svelte-marked';
-	import SvelteMarkdown from 'svelte-markdown';
-
 	import { page } from '$app/stores';
-
-	const source = `
-  # This is a header
-
-This is a paragraph.
-
-* This is a list
-* With two items
-  1. And a sublist
-  2. That is ordered
-    * With another
-    * Sublist inside
-
-| And this is | A table |
-|-------------|---------|
-| With two    | columns |
-`;
-
-	const options = { langPrefix: 'markdown-body' };
 
 	const modules = import.meta.glob('$lib/content/listings/*.json', { eager: true });
 	const listing = modules[`/src/lib/content/listings/${$page.params.listing}.json`];
+	// const listing = source;
 </script>
 
-<div class="prose">
-	<SvelteMarkdown source="{source}" options="{options}" />
-</div>
+<div>
+	<section>
+		<h3>Description</h3>
+		<p class="prose main-paragraph">
+			{@html listing.description}
+		</p>
+	</section>
 
-<div class="prose">
-	<Markdown
-		source="{`
-# Setup
-Here are the steps to set up svelte-marked plugin
-1. Install it
-2. Enjoy
-    - If you enjoyed, make sure to star the repo!
-`}"
-	/>
-</div>
+	<section>
+		<h3>Details</h3>
+		<p class="prose main-paragraph">
+			{@html listing.details}
+		</p>
+	</section>
 
-<h1>{$page.params.listing}</h1>
-<div class="p-5">
-	<a href="{listing.url}" class="text-xl font-bold">{listing.title}</a>
-	<p class="mt-1 text-sm">{listing.body}</p>
-	<img src="{listing.thumbnail}" alt="" class="h-40" />
+	<section>
+		<h3>Summary</h3>
+		<div>
+			<div>
+				<div>
+					<h4>Property</h4>
+					{#each ['type', 'road_frontage'] as attribute}
+						{#if attribute in listing.summary.property}
+							<h5 class="">{attribute}</h5>
+							<div>{listing.summary.property[attribute]}</div>
+						{/if}
+					{/each}
+				</div>
+				<div> </div>
+			</div>
+			<div>
+				<div> </div>
+				<div> </div>
+				<div> </div>
+			</div>
+		</div>
+	</section>
+
+	<div class="p-5">
+		<h2>{listing.title}</h2>
+		<img src="{listing.thumbnail}" alt="" class="h-40" />
+	</div>
 </div>
