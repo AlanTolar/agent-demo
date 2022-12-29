819be77a76ddab22c9f6abcc865badda73b242f6
Alan <test@test>
2022-12-28 22:39:42 -0500

    top bar for map

--- src/routes/(app)/listings/+page.svelte
+++ src/routes/(app)/listings/+page.svelte
@@ -113,129 +113,129 @@
 	}
 </script>
 
-<div class="flex h-[calc(100vh-64px)]">
-	<div class="grow flex flex-col">
-		<menu class="flex justify-around items-center py-2 border-b-2">
-			<li class="relative w-1/3">
-				<Slider
-					min="{minPrice}"
-					max="{maxPrice}"
-					name="Price"
-					prefix="$"
-					on:stop="{(e) => {
-						minPriceValue = e.detail.values[0];
-						maxPriceValue = e.detail.values[1];
-					}}"
-				/>
-			</li>
-			<li class="relative w-1/3">
-				<Slider
-					min="{minAcres}"
-					max="{maxAcres}"
-					name="Acres"
-					on:stop="{(e) => {
-						console.log(e);
-						minAcresValue = e.detail.values[0];
-						maxAcresValue = e.detail.values[1];
-					}}"
-				/>
-			</li>
-			<li class="relative">
-				<button
-					class="text-label font-semibold text-primary-600 flex"
-					on:click="{() => (showFilterDropdown = !showFilterDropdown)}"
-					>Filter By<iconify-icon
-						inline
-						icon="material-symbols:keyboard-arrow-down"
-						width="24"></iconify-icon></button
-				>
-				{#if showFilterDropdown}
-					<form class="absolute flex bg-white right-0 z-10 rounded-lg py-4 divide-x">
-						<fieldset class="flex flex-col whitespace-nowrap gap-2 px-4">
-							<span class="font-semibold">Land Type</span>
-							<InputField name="hunting" title="Hunting" />
-							<InputField name="home" title="Home" />
-							<InputField name="farm-and-ranch" title="Farm & Ranch" />
-						</fieldset>
-						<fieldset class="flex flex-col whitespace-nowrap gap-2 px-4">
-							<span class="font-semibold">Availability</span>
-							<InputField name="available" title="Available" />
-							<InputField name="under-contract" title="Under Contract" />
-							<InputField name="sold" title="Sold" />
-						</fieldset>
-					</form>
-				{/if}
-			</li>
-		</menu>
-		<Map
-			accessToken="pk.eyJ1IjoibGFuZGxpc3Rpbmdwcm8iLCJhIjoiY2tuNjQ2djRxMGFkczJ3cXBxcmd4a2VnYSJ9.1bw7SeYN6vx3TIj849l5CA"
-			bind:this="{mapComponent}"
-			on:ready="{() => initMap()}"
-			center="{center}"
-			zoom="2"
-			options="{{ scrollZoom: false }}"
-		>
-			{#each listings as listing}
-				{#if listing.location && listing.visible}
-					<Marker
-						lng="{listing.location.coordinates[0]}"
-						lat="{listing.location.coordinates[1]}"
-						color="rgb(0,255,255)"
-						popupOptions="{{ closeButton: false, focusAfterOpen: false }}"
-					>
-						<div class="" slot="popup">
-							<ListingCard
-								listing="{listing}"
-								extraClasses="min-h-[150px]"
-								horizontal="{false}"
-							/>
-						</div>
-					</Marker>
-				{/if}
-			{/each}
-			<NavigationControl />
-			<ScaleControl />
-		</Map>
-	</div>
-
-	<div class="overflow-scroll w-1/3 xl:w-[500px] border-l-2">
-		<menu
-			class="flex flex-col xl:flex-row gap-2 justify-between sticky top-0 z-40 bg-neutral-200 px-6 py-4"
-		>
-			<li class="text-primary-600 font-semibold"
-				><span>{listings.filter((obj) => obj.visible).length}</span> listings</li
+<div class="">
+	<menu class="flex justify-around items-center h-[80px] gap-6 bg-neutral-100 border-b-2">
+		<li class="relative w-1/5">
+			<Slider
+				min="{minPrice}"
+				max="{maxPrice}"
+				name="Price"
+				prefix="$"
+				on:stop="{(e) => {
+					minPriceValue = e.detail.values[0];
+					maxPriceValue = e.detail.values[1];
+				}}"
+			/>
+		</li>
+		<li class="relative w-1/5">
+			<Slider
+				min="{minAcres}"
+				max="{maxAcres}"
+				name="Acres"
+				on:stop="{(e) => {
+					console.log(e);
+					minAcresValue = e.detail.values[0];
+					maxAcresValue = e.detail.values[1];
+				}}"
+			/>
+		</li>
+		<li class="relative">
+			<button
+				class="text-label font-semibold text-primary-600 flex"
+				on:click="{() => (showFilterDropdown = !showFilterDropdown)}"
+				>Filter By<iconify-icon
+					inline
+					icon="material-symbols:keyboard-arrow-down"
+					width="24"></iconify-icon></button
 			>
-			<li>
-				<select
-					class="h-full rounded-md border-transparent bg-neutral-300 py-0 main-text"
-					on:change="{(e) => e.target && changeOrder(e.target)}"
-				>
-					<option value="new-to-old">Date: New to Old</option>
-					<option value="old-to-new">Date: Old to New</option>
-					<option value="small-to-large">Acres: Small to Large</option>
-					<option value="large-to-small">Acres: Large to Small</option>
-					<option value="low-to-high">Price: Low to High</option>
-					<option value="high-to-low">Price: High to Low</option>
-					<option value="a-to-z">Name: A to Z</option>
-					<option value="z-to-a">Name: Z to A</option>
-				</select>
-			</li>
-		</menu>
-		<div class="p-6 grid grid-cols-1 grid-flow-row gap-6">
-			{#each listings as listing}
-				{#if listing.visible}
-					<ListingCard
-						listing="{listing}"
-						extraClasses="xl:hidden"
-						horizontal="{false}"
-					/>
-					<ListingCard
-						listing="{listing}"
-						extraClasses="min-h-[130px] max-xl:hidden"
-						horizontal="{true}"
-					/>
-				{/if}
-			{/each}
+			{#if showFilterDropdown}
+				<form class="absolute flex bg-white right-0 z-10 rounded-lg py-4 divide-x">
+					<fieldset class="flex flex-col whitespace-nowrap gap-2 px-4">
+						<span class="font-semibold">Land Type</span>
+						<InputField name="hunting" title="Hunting" />
+						<InputField name="home" title="Home" />
+						<InputField name="farm-and-ranch" title="Farm & Ranch" />
+					</fieldset>
+					<fieldset class="flex flex-col whitespace-nowrap gap-2 px-4">
+						<span class="font-semibold">Availability</span>
+						<InputField name="available" title="Available" />
+						<InputField name="under-contract" title="Under Contract" />
+						<InputField name="sold" title="Sold" />
+					</fieldset>
+				</form>
+			{/if}
+		</li>
+
+		<li>
+			<select
+				class="h-full rounded-md border-transparent bg-neutral-200 py-0 main-text"
+				on:change="{(e) => e.target && changeOrder(e.target)}"
+			>
+				<option value="new-to-old">Date: New to Old</option>
+				<option value="old-to-new">Date: Old to New</option>
+				<option value="small-to-large">Acres: Small to Large</option>
+				<option value="large-to-small">Acres: Large to Small</option>
+				<option value="low-to-high">Price: Low to High</option>
+				<option value="high-to-low">Price: High to Low</option>
+				<option value="a-to-z">Name: A to Z</option>
+				<option value="z-to-a">Name: Z to A</option>
+			</select>
+		</li>
+		<li class="text-primary-600 font-semibold"
+			><span>{listings.filter((obj) => obj.visible).length}</span> listings</li
+		>
+	</menu>
+
+	<div class="flex h-[calc(100vh-64px-80px)]">
+		<div class="grow">
+			<Map
+				accessToken="pk.eyJ1IjoibGFuZGxpc3Rpbmdwcm8iLCJhIjoiY2tuNjQ2djRxMGFkczJ3cXBxcmd4a2VnYSJ9.1bw7SeYN6vx3TIj849l5CA"
+				bind:this="{mapComponent}"
+				on:ready="{() => initMap()}"
+				center="{center}"
+				zoom="2"
+				options="{{ scrollZoom: false }}"
+			>
+				{#each listings as listing}
+					{#if listing.location && listing.visible}
+						<Marker
+							lng="{listing.location.coordinates[0]}"
+							lat="{listing.location.coordinates[1]}"
+							color="rgb(0,255,255)"
+							popupOptions="{{ closeButton: false, focusAfterOpen: false }}"
+						>
+							<div class="" slot="popup">
+								<ListingCard
+									listing="{listing}"
+									extraClasses="min-h-[150px]"
+									horizontal="{false}"
+								/>
+							</div>
+						</Marker>
+					{/if}
+				{/each}
+				<NavigationControl />
+				<ScaleControl />
+			</Map>
+		</div>
+
+		<div class="overflow-scroll w-1/3 xl:w-[500px] border-l-2 bg-neutral-200">
+			<div class="p-6 grid grid-cols-1 grid-flow-row gap-6">
+				{#each listings as listing}
+					{#if listing.visible}
+						<ListingCard
+							listing="{listing}"
+							extraClasses="xl:hidden"
+							horizontal="{false}"
+						/>
+						<ListingCard
+							listing="{listing}"
+							extraClasses="min-h-[130px] max-xl:hidden"
+							horizontal="{true}"
+						/>
+					{/if}
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
