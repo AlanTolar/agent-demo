d8c53d3df1ba2062c62549498a01b25ebbf58396
Alan Tolar <29237222+AlanTolar@users.noreply.github.com>
2023-03-19 15:07:29 -0400

    Subscribe form works and some pages prerender

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,10 +1,10 @@
 import type { PageLoad } from './$types';
 import { getListings } from '$lib/utils/cmsDataHandler';
 
+export const prerender = true;
+
 export const load = (({ params }) => {
 	return {
 		listings: getListings(),
 	};
 }) satisfies PageLoad;
-
-export const prerender = true;
--- src/routes/(app)/listings/+page.ts
+++ src/routes/(app)/listings/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from './$types';
 import { getListings } from '$lib/utils/cmsDataHandler';
 
+export const prerender = true;
+
 export const load = (({ params }) => {
 	return {
 		listings: getListings(),
