3c7eb1bb5136f0cdca4aac8e1cb0c74ab5b5ebe8
Alan Tolar <29237222+AlanTolar@users.noreply.github.com>
2023-04-02 23:27:34 -0400

    CMS works

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-	import CMS from '@staticcms/core';
 	import { config } from './config';
+	import { onMount } from 'svelte';
 
-	// Initialize the CMS object
-	CMS.init({ config });
-	// Now the registry is available via the CMS object.
-	// CMS.registerPreviewTemplate('my-template', MyTemplate);
+	onMount(async () => {
+		// Initialize the CMS object
+		console.log('window.CMSLoaded: ', window.CMSLoaded);
+		if (!window.CMSLoaded) window.CMS.init({ config });
+		window.CMSLoaded = true;
+		// Now the registry is available via the CMS object.
+		// CMS.registerPreviewTemplate('my-template', MyTemplate);
+	});
 </script>
+
+<svelte:head>
+	<script src="https://unpkg.com/@staticcms/app@^1.2.14/dist/static-cms-app.js"></script>
+</svelte:head>
